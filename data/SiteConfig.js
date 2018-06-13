ae46220cc0c5c5c251920126230637e76f782913
Михаил Синяков <pvpshoot@gmail.com>
2018-06-13 13:59:49 +0400

    add site description

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,28 +1,27 @@
 module.exports = {
   blogPostDir: "sample-posts", // The name of directory that contains your posts.
   blogAuthorDir: "sample-authors", // The name of directory that contains your 'authors' folder.
-  blogAuthorId: "casper", // The default and fallback author ID used for blog posts without a defined author.
-  siteTitle: "Gatsby Casper Starter", // Site title.
-  siteTitleAlt: "GatsbyJS Casper Theme Starter", // Alternative site title for SEO.
+  blogAuthorId: "Vladimir", // The default and fallback author ID used for blog posts without a defined author.
+  siteTitle: "UrbanRuTrip", // Site title.
+  siteTitleAlt: "UrbanRuTrip", // Alternative site title for SEO.
   siteLogo:
-    "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
+    "/logos/logo.jpg", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
   siteUrl: "https://pvpshoot.github.io/", // Domain of your website without pathPrefix.
   pathPrefix: "/urbantrip", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription:
-    "A Casper themed GatsbyJS stater based on the Advanced Starter.", // Website description used for RSS feeds/meta description tag.
+    "О желании рассказать о своем трипе писать @urbanlich. Рекомендации по вещанию http://telegra.ph/Urban-RuTrip-05-28", // Website description used for RSS feeds/meta description tag.
   siteCover:
     "https://haysclark.github.io/gatsby-starter-casper/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
   siteNavigation: true, // If navigation is enabled the Menu button will be visible
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteRssAuthor: "Casper User", // The author name used in the RSS file
+  siteRssAuthor: "pvpshoot", // The author name used in the RSS file
   // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
   sitePaginationLimit: 10, // The max number of posts per page.
   googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
   // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
   siteSocialUrls: [
-    "https://github.com/haysclark/gatsby-starter-casper",
-    "https://twitter.com/gatsbyjs",
-    "mailto:gatsbyjs@example.com"
+    "https://github.com/pvpshoot/urbantrip",
+    "https://t.me/UrbanRuTrip",
   ],
   postDefaultCategoryID: "Tech", // Default category for posts.
   // Links to social profiles/projects you want to display in the navigation bar.
